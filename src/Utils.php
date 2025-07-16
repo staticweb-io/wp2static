2ece2ca7c695506dc9470bbe83ae57a849e55072
John Shaffer <jdsha@pm.me>
2025-07-16 15:26:14 -0500

    Fix max_execution_time being set to 30 and not 0
    Backport of
    https://github.com/staticweb-io/static-deploy/commit/525fd1213182f2903671e64c184be97ec298367a

--- src/Utils.php
+++ src/Utils.php
@@ -58,11 +58,11 @@ class Utils {
             return;
         }
 
-        $current_max_execution_time  = ini_get( 'max_execution_time' );
+        $current_max_execution_time  = intval( ini_get( 'max_execution_time' ) );
         $proposed_max_execution_time =
             ( $current_max_execution_time === 30 ) ? 31 : 30;
         set_time_limit( $proposed_max_execution_time );
-        $current_max_execution_time = ini_get( 'max_execution_time' );
+        $current_max_execution_time = intval( ini_get( 'max_execution_time' ) );
 
         if ( $proposed_max_execution_time === $current_max_execution_time ) {
             set_time_limit( 0 );
